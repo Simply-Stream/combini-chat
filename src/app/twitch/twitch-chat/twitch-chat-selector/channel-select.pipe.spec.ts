3e3396514f6a402da68a9e9ba0df62b33ea5ff7a
AaricDev <aaricdev@simply-stream.com>
2021-05-13 23:16:16 +0200

    feat: Remove some tests

--- src/app/twitch/twitch-chat/twitch-chat-selector/channel-select.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChannelSelectPipe } from './channel-select.pipe';
-
-describe('ChannelSelectPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ChannelSelectPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
